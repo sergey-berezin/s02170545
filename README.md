6cfab04f4f215a05a0cc2444e0be373bc6da9b2e
Eytlin <dartlevonchic@gmail.com>
2020-10-04 15:59:24 +0300

    Initial+1

---
+++ README.md
@@ -0,0 +1,6 @@
+Requirements
+------------
+
+* [ONNX ResNet Model](https://github.com/onnx/models/blob/master/vision/classification/resnet/model/resnet50-v2-7.onnx) to be placed into Data directory  and renamed into model.onnx
+* Images to be placed into Data/images directory
+ 
---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using ResNetMatcher;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace Task1 {
+    class Program {
+
+        static void Callback(string image, int id, float prob) {
+            // I/O operations using these streams are synchronized, 
+            //  which means multiple threads can read from, or write to, 
+            //  the streams.
+            Console.WriteLine($"{image} is: {Labels.classLabels[id]} with: {prob}");
+        }
+
+        static void Main(string[] args) {
+            Matcher matcher = new Matcher();
+
+            try {
+                if (args.Length == 0)
+                    matcher.Match(Callback, "Data");
+                else
+                    matcher.Match(Callback, args[0]);
+
+                Console.WriteLine($"Data path is: {(args.Length == 0 ? "Data" : args[0])}");
+                Console.WriteLine("Press ESC to exit");
+                while (Console.ReadKey().Key != ConsoleKey.Escape)
+                    ;
+
+                matcher.CancelMatch();
+            } catch (FileNotFoundException e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
