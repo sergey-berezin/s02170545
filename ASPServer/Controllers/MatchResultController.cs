032d9489487d3a96c218256fcef633743e4cccad
Eytlin <bitrate16@gmail.com>
2020-12-16 11:22:36 +0300

    Task 5

--- ASPServer/Controllers/MatchResultController.cs
+++ ASPServer/Controllers/MatchResultController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Diagnostics;
 
 namespace ASPServer.Controllers {
 
--- ASPServer/Controllers/WebappController.cs
+++ ASPServer/Controllers/WebappController.cs
@@ -9,16 +9,13 @@ using System.Net.Http.Headers;
 namespace ASPServer.Controllers {
 
     [ApiController]
-    [Route("api/weball")]
-    class WebappController : ControllerBase {
+    [Route("[controller]")]
+    public class WebappController : ControllerBase {
 
         // TODO: Не обрабатывает запрос
 
         [HttpGet]
         public ActionResult GetWebApp() {
-            Console.WriteLine("AAAAA0");
-            Trace.WriteLine("AAAAA0");
-            return base.Content("AAAAAAAAAAAAAAAAA", "text/html");
             try {
                 return base.Content(System.IO.File.ReadAllText("WEB/index.html"), "text/html");
             } catch (Exception e) {
